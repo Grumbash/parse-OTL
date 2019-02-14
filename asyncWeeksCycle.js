eceecbc1c0f000fe38c25cad35cb1e7dfd68575e
Rossev Gleb <rossevgleb@gmail.com>
2019-02-14 17:39:22 +0300

    Get data in month
    In dev: get back function, save data, update data

--- asyncWeeksCycle.js
+++ asyncWeeksCycle.js
@@ -1,24 +1,48 @@
-const parsing = require('./src/parsing');
+const parsingMonth = require('./src/parsing');
 const User = require("./src/models/User");
 
 module.exports = async ({ USER_NAME, USER_PASSWORD }, URL) => {
   console.log("Parsing start");
   try {
-    const weekNo = 1;
-    const user = {
-      name: USER_NAME,
-      period: await parsing({
-        URL,
-        USER_NAME,
-        USER_PASSWORD
-      }, weekNo)
+    const user = await User.findOne({ name: USER_NAME });
+    if (user.period.length === 0) {
+      // get All info
+      const weekNo = 1;
+      const userToUpdate = {
+        name: USER_NAME,
+        period: await parsingAll({
+          URL,
+          USER_NAME,
+          USER_PASSWORD
+        }, weekNo)
+      }
+
+      const result = await new User(userToUpdate).save();
+
+      console.log(result);
+
+      return result;
+
+    } else {
+      // get Last month info
+      const userToUpdate = {
+        name: USER_NAME,
+        period: await parsingMonth({
+          URL,
+          USER_NAME,
+          USER_PASSWORD
+        })
+      }
+
+      await User.findByIdAndUpdate(user.id, userToUpdate);
+
+      const result = await User.findById(user.id);
+
+      console.log(result);
+
+      return result;
     }
 
-    const result = await new User(user).save();
-
-    console.log(result);
-
-    return result;
 
   } catch (error) {
     console.log(error)
--- src/parsing.js
+++ src/parsing.js
@@ -1,9 +1,11 @@
 const puppeteer = require('puppeteer');
 const { setTextInputValue } = require("./shared/setInputTextValue");
 const getDataFromPage = require("./controllers/getDataFromPage");
+const getOTL_data = require("./controllers/getOTL_data");
+const getDataFromExpandTable = require("./controllers/getDataFromExpandTable");
+const moment = require("moment");
 
-
-async function parsing({ URL, USER_NAME, USER_PASSWORD }, weekNo) {
+async function parsing({ URL, USER_NAME, USER_PASSWORD }) {
   try {
     const browser = await puppeteer.launch({
       headless: true, defaultViewport: {
@@ -23,13 +25,38 @@ async function parsing({ URL, USER_NAME, USER_PASSWORD }, weekNo) {
     await page.waitForSelector(".flat-grid-cell>.app-nav-item svg[data-icon='navi_ledgerclock']", { visible: true });
     await page.click(".flat-grid-cell>.app-nav-item svg[data-icon='navi_ledgerclock']");
 
-    // Get week from DB
-    // Data processing 
-    const periods = [];
-    // Why I use weeks? It must be something else 
-    for (let week = 1; week <= weekNo; week++) {
-      periods.push(await getDataFromPage(week, page));
-    }
+    await page.evaluate(() => {
+      const fromInput = document.querySelectorAll("input[id]")[3];
+      const toInput = document.querySelectorAll("input[id]")[6];
+      const searchButton = document.querySelectorAll("td[class] > button[id]")[6];
+      const firstDAyOfMonth = moment(new Date).startOf('month').format("MM/DD/YY");
+      const lastDayOfMonth = moment(new Date).endOf('month').format("MM/DD/YY");
+
+      fromInput.value = firstDAyOfMonth;
+      toInput.value = lastDayOfMonth;
+      searchButton.click()
+    });
+
+    await page.waitForSelector("table[summary='Search Results:Time Cards'] > tbody > tr", { visible: true });
+
+    const weeks = await page.evaluate(() => [...document.querySelectorAll("table[summary='Search Results:Time Cards'] > tbody > tr")]);
+
+    weeks.forEach(async (elem, indx) => {
+      const OTL_data = await getOTL_data(page, indx);
+      let fullInfoSelector = `table[summary='Search Results:Time Cards'] > colgroup[span] + tbody > tr:nth-child(${weekNo}) > td:nth-child(10) > span > a`;
+      await page.waitForSelector(fullInfoSelector, { visible: true });
+      await page.evaluate((selector) => {
+        let toClick = document.querySelector(selector);
+        toClick.click();
+      }, fullInfoSelector);
+
+      // Get data for the week
+      let ExpandTableSelector = "table[summary='Time Card Entries'] tr.xeq > td:nth-child(2) tr";
+      const projects = await getDataFromExpandTable({ page, selector: ExpandTableSelector });
+      await goBack()
+      return { ...OTL_data, projects };
+    });
+
 
     browser.close();
 
