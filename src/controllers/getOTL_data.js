e2c24d80e63e396f1bf89a55d86ebecc26d56940
Rossev Gleb <rossevgleb@gmail.com>
2019-02-18 18:34:33 +0300

    Timestamp added, data format changed

--- src/controllers/getOTL_data.js
+++ src/controllers/getOTL_data.js
@@ -1,9 +1,7 @@
-const moment = require("moment");
-
 module.exports = async (page, firtsRow = 0) => {
   await page.waitForSelector("table[summary='Search Results:Time Cards'] > colgroup[span] + tbody");
 
-  const lastPeriod = await page.evaluate((firtsRow) => {
+  return await page.evaluate((firtsRow) => {
     // Get first row data
     const period = document.querySelectorAll("table[summary='Search Results:Time Cards'] > colgroup[span] + tbody > tr > td:nth-child(1)")[firtsRow].textContent;
     const [from, to] = period.split(" - ");
@@ -15,9 +13,4 @@ module.exports = async (page, firtsRow = 0) => {
     }
   }, firtsRow);
 
-  return {
-    status: lastPeriod.status,
-    from: moment(lastPeriod.from, 'MM/DD/YY'),
-    to: moment(lastPeriod.to, 'MM/DD/YY')
-  };
 }
--- src/models/User.js
+++ src/models/User.js
@@ -1,20 +1,25 @@
 const { Schema, model } = require("mongoose");
 
-const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  periods: [{
-    from: Date,
-    to: Date,
-    status: String,
-    projects: [{
-      PO: Number,
-      name: String,
-      days: [Number]
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    periods: [{
+      from: String,
+      to: String,
+      status: String,
+      projects: [{
+        PO: Number,
+        name: String,
+        days: [Number]
+      }]
     }]
-  }]
-});
+  },
+  {
+    timestamps: true
+  }
+);
 
 module.exports = User = model("users", UserSchema, "users");
