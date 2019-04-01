591d206690be0701bb7a263f65867a67942ae971
Rossev Gleb <rossevgleb@gmail.com>
2019-04-01 16:32:12 +0300

    Bug fix: null of periods

--- asyncWeeksCycle.js
+++ asyncWeeksCycle.js
@@ -1,62 +1,66 @@
-const parsingMonth = require('./src/parsing');
-const User = require("./src/models/User");
-const logger = require("./logger");
+const parsingMonth = require ('./src/parsing');
+const User = require ('./src/models/User');
+const logger = require ('./logger');
 
-module.exports = async ({ USER_NAME, USER_PASSWORD }, URL) => {
-
-  logger.info({ message: `${USER_NAME} -- parsing start` });
+module.exports = async ({USER_NAME, USER_PASSWORD}, URL) => {
+  logger.info ({message: `${USER_NAME} -- parsing start`});
 
   try {
-    let user = await User.findOne({ name: USER_NAME });
-    const parsingRes = async (userInner) => await parsingMonth({
-      URL,
-      USER_NAME,
-      USER_PASSWORD
-    }, userInner.id);
+    let user = await User.findOne ({name: USER_NAME});
+    const parsingRes = async userInner =>
+      await parsingMonth (
+        {
+          URL,
+          USER_NAME,
+          USER_PASSWORD,
+        },
+        userInner.id
+      );
 
     if (!user) {
-      const user = await new User({ name: USER_NAME }).save();
+      const user = await new User ({name: USER_NAME}).save ();
 
-      logger.info({ message: `Add ${user.name} as new in DB` });
+      logger.info ({message: `Add ${user.name} as new in DB`});
 
-      const res = await parsingRes(user);
+      const res = await parsingRes (user);
       const userToUpdate = {
         name: USER_NAME,
         periods: res.periods,
-        screenshot: res.screen
-      }
-      await User.findByIdAndUpdate(user.id, userToUpdate);
-      const result = await User.findById(user.id);
+        screenshot: res.screen,
+      };
+      await User.findByIdAndUpdate (user.id, userToUpdate);
+      const result = await User.findById (user.id);
 
-      logger.info({ message: `User ${result.name} has been created` })
+      logger.info ({message: `User ${result.name} has been created`});
 
       return result;
-
     } else {
       // get Last month info
-      const res = await parsingRes(user);
+      const res = await parsingRes (user);
       const userToUpdate = {
         name: USER_NAME,
         periods: res.periods,
-        screenshot: res.screen
-      }
+        screenshot: res.screen,
+      };
 
-      const userOld = await User.findById(user.id);
+      const userOld = await User.findById (user.id);
       const arrOfPeriods = [...userToUpdate.periods, ...userOld.periods];
-      userToUpdate.periods = arrOfPeriods.filter((periodId, index, self) => index === self.findIndex((innerPeriodId) => innerPeriodId == periodId));
+      userToUpdate.periods = arrOfPeriods.filter (
+        (periodId, index, self) =>
+          index === self.findIndex (innerPeriodId => innerPeriodId == periodId)
+      );
 
-      await User.findByIdAndUpdate(user.id, userToUpdate);
+      await User.findByIdAndUpdate (user.id, userToUpdate);
 
-      logger.info({ message: `Update  ${user.name} in DB` });
+      logger.info ({message: `Update  ${user.name} in DB`});
 
-      const result = await User.findById(user.id);
+      const result = await User.findById (user.id);
 
-      logger.info({ message: `User ${result.name} has been updated` });
+      logger.info ({message: `User ${result.name} has been updated`});
 
       return result;
     }
-
   } catch (error) {
-    logger.error(error)
+    logger.error (error);
   }
-}
+};
